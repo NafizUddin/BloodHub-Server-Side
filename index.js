aeb86c8025f877df464aba69ad6c8913f76445ad
NafizUddin <nafizuddin.ctg@gmail.com>
2023-12-04 19:08:53 +0600

    added client live server in cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"], // Client Side Server
+    origin: ["https://bloodhub-fullstack-a12.netlify.app"], // Client Side Server
     credentials: true,
   })
 );
